f64977073f10d3888ec50d5a1da3e36676817eda
Olivier D <delaporteol@me.com>
2022-12-17 12:32:50 +0100

    fix(designSystem): force unwrap colors

--- Sources/DesignSystem/Color.swift
+++ Sources/DesignSystem/Color.swift
@@ -10,12 +10,12 @@ import Foundation
 
 public enum Color {
 
-    public static let background = UIColor(named: "Background", in: Bundle.module, compatibleWith: nil)
+    public static let background = UIColor(named: "Background", in: Bundle.module, compatibleWith: nil)!
 
-    public static let primary = UIColor(named: "Primary", in: Bundle.module, compatibleWith: nil)
+    public static let primary = UIColor(named: "Primary", in: Bundle.module, compatibleWith: nil)!
 
-    public static let secondary = UIColor(named: "Secondary", in: Bundle.module, compatibleWith: nil)
+    public static let secondary = UIColor(named: "Secondary", in: Bundle.module, compatibleWith: nil)!
 
-    public static let blue = UIColor(named: "Blue", in: Bundle.module, compatibleWith: nil)
+    public static let blue = UIColor(named: "Blue", in: Bundle.module, compatibleWith: nil)!
 
 }
