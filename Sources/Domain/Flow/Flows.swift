260c6ac3612fad565c94743414b4f9ba3fa5c53c
Olivier D <delaporteol@me.com>
2022-12-17 12:04:51 +0100

    feat(Flows): introduce flow protocols in Domain module

---
+++ Sources/Domain/Flow/Flows.swift
@@ -0,0 +1,12 @@
+//
+//  Flows.swift
+//  
+//
+//  Created by Grinch on 17/12/2022.
+//
+
+import Foundation
+
+public protocol ListingPicturesFlow: AnyObject {
+    func showPictureDetail(id: String)
+}
